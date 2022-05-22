d106d87eb33bb953b8f357146f4af6b70a605eb2
habulCoderJohnny <fokruljohnny@gmail.com>
2022-05-22 23:17:48 +0600

    Implement signInWithEmailAndPassword & Displayed error massage and Loading Spinner

--- src/PAGES/AUTH/Login.js
+++ src/PAGES/AUTH/Login.js
@@ -1,8 +1,38 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
 import logo from '../../assets/images/logo.png';
+import auth from '../../firebase.init';
+import Loading from '../SHARED/Loading/Loading';
+import ErrorMassages from '../SHARED/Loading/ErrorMassages';
 
 const Login = () => {
+    const { register, formState: { errors }, handleSubmit} = useForm();
+    const navigate = useNavigate()
+    let signInError;
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    if (user) {
+        navigate('/')
+    }
+
+    if (error) {
+        signInError = <ErrorMassages>{error?.message}</ErrorMassages>
+    }
+
+
+    const onSubmit = data => {
+        console.log(data);
+        signInWithEmailAndPassword(data.email, data.password);
+        console.log(data.email);
+    }
 
 
     return (
@@ -13,13 +43,28 @@ const Login = () => {
                     <h1 className="text-primary font-bold pl-10"><span className='text-[#354069]'> Hello, </span>Welcome Back!</h1>
                 </div>
                 <div className="card-body">
-                    <form className="text-blue-600">
+                    <form onSubmit={handleSubmit(onSubmit)} className="text-blue-600">
                         <div className="form-control">
                             <label className="label p-1">
                                 <span className="label-text text-xl">Email</span>
                             </label>
 
-                            <input type="email" placeholder="email" className="input input-secondary text-xl" />
+                            <input type="email" placeholder="email" className="input input-secondary text-xl" {...register("email", {
+                                required: {
+                                    value: true,
+                                    message: 'Email is Required'
+                                },
+                                pattern: {
+                                    value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                    message: 'Provide a valid Email'
+                                }
+                            })} />
+
+
+                            <label className="label p-1">
+                                {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                                {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                            </label>
                         </div>
 
 
@@ -28,8 +73,27 @@ const Login = () => {
                                 <span className="label-text text-xl">Password</span>
                             </label>
 
-                            <input type="password" placeholder="password" className="input input-error text-xl" />
+                            <input type="password" placeholder="password" className="input input-error text-xl"{...register("password", {
+                                required: {
+                                    value: true,
+                                    message: 'Password is Required'
+                                },
+                                minLength: {
+                                    value: 6,
+                                    message: 'Must be 6 characters or longer'
+                                }
+                            })} />
+
+
+                            <label className="label">
+                                {errors.password?.type === 'required' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+                                {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+                            </label>
                         </div>
+
+                        {signInError} 
+                        { loading && <Loading></Loading>}
+
 
                         <div className="form-control">
                             <input type="submit" value="Login" className="btn btn-primary text-white
